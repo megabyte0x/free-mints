0a375449edb08a11a3b81031636856033f27f449
Yash Garg <yashygkf@gmail.com>
2022-07-29 16:51:57 +0530

    add: wrote test to check the minting and assigning uri

--- backend/test/index.ts
+++ backend/test/index.ts
@@ -1,19 +1,21 @@
 import { expect } from "chai";
 import { ethers } from "hardhat";
 
-describe("Greeter", function () {
-  it("Should return the new greeting once it's changed", async function () {
-    const Greeter = await ethers.getContractFactory("Greeter");
-    const greeter = await Greeter.deploy("Hello, world!");
-    await greeter.deployed();
+describe("Mint", function () {
+  it("Should mint an NFT", async function () {
+    const accounts = await ethers.getSigners();
+    const user = accounts[1];
 
-    expect(await greeter.greet()).to.equal("Hello, world!");
+    const FreeMints = await ethers.getContractFactory("FreeMints");
+    const freeMints = await FreeMints.deploy();
+    await freeMints.deployed();
 
-    const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
+    expect(await freeMints.uriMap(user.address, "0")).to.be.equal("");
 
-    // wait until the transaction is mined
-    await setGreetingTx.wait();
+    await freeMints.mint(user.address, "something.xyz");
 
-    expect(await greeter.greet()).to.equal("Hola, mundo!");
+    expect(await freeMints.uriMap(user.address, "0")).to.be.equal(
+      "something.xyz"
+    );
   });
 });
