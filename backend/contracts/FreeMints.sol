d076d883f786f2e32534aa08628e8658ce7b0cf7
Yash Garg <yashygkf@gmail.com>
2022-07-29 16:51:06 +0530

    add: Event added

--- backend/contracts/FreeMints.sol
+++ backend/contracts/FreeMints.sol
@@ -12,17 +12,22 @@ contract FreeMints is ERC721URIStorage, Ownable {
     Counters.Counter private _tokenId;
 
     //* Address => Their Minted NFT (TokenId => Token URI)
-    mapping(address => mapping(uint256 => string)) private uriMap;
+    mapping(address => mapping(uint256 => string)) public uriMap;
 
     constructor() ERC721("Free Mints", "FM") {}
+
+    event Minted(address indexed to, uint256 indexed tokenId, string indexed uri);
 
     function mint(address _to, string memory _tokenURI) external {
         uint256 tokenId = _tokenId.current();
 
         _safeMint(_to, tokenId);
         _setTokenURI(tokenId, _tokenURI);
-        _tokenId.increment();
+
         uriMap[_to][tokenId] = _tokenURI;
+        
+        emit Minted(_to, tokenId, _tokenURI);
+
+        _tokenId.increment();
     }
 }
-
